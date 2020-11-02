1972b6d261cbcfb8ea4ae6b458157f0f1e3322cf
ASUS <sonitkach@gmail.com>
2020-11-02 16:24:17 +0300

    add crud for posts

--- photoalbum/photoalbum_site/urls.py
+++ photoalbum/photoalbum_site/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('post/<str:pk>', views.post, name='post'),
     path('posts/', views.posts, name='posts'),
     path('profile/', views.profile, name='profile'),
+
+    # crud
+
+    path('create_post/', views.createPost, name='create_post'),
+    path('update_post/<str:pk>/', views.updatePost, name='update_post'),
+    path('delete_post/<str:pk>/', views.deletePost, name='delete_post'),
+
 ]
--- photoalbum/photoalbum_site/views.py
+++ photoalbum/photoalbum_site/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 from .models import Post
+from .forms import PostForm
 
 def post (request,pk):
     post = Post.objects.get(id=pk)
@@ -21,4 +23,40 @@ def home(request):
 def profile (request):
     return render(request, 'photoalbum_site/profile.html')
 
+# crud
 
+@login_required(login_url='home')
+def createPost(request):
+    form = PostForm
+
+    if request.method=='POST':
+        form=PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('posts')
+
+    context={'form':form}
+    return render (request, 'photoalbum_site/post_form.html',context)
+
+@login_required(login_url='home')
+def updatePost(request, pk):
+    post = Post.objects.get(id=pk)
+    form = PostForm(instance=post)
+
+    if request.method=='POST':
+        form=PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+        return redirect('posts')
+
+    context={'form':form}
+    return render (request, 'photoalbum_site/post_form.html',context)
+
+@login_required(login_url='home')
+def deletePost(request, pk):
+    post = Post.objects.get(id=pk)
+    if request.method=='POST':
+        post.delete()
+        return redirect('posts')
+    context = {'item':post}
+    return render(request, 'photoalbum_site/delete.html', context)
